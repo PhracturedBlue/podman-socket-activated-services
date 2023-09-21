7eda9d844dde9270e5de15155719874efd09574a
PhracturedBlue <rc2012@pblue.org>
2023-09-21 03:56:16 -0700

    Kopia native support

--- kopia/README.md
+++ kopia/README.md
@@ -1,21 +1,17 @@
 # Kopia
 
-Kopia includes unix-domain-socket support (not yet released), but not socket-activation support.
-However, a patch for socket-activation support has been submitted [here](https://github.com/kopia/kopia/pull/3283)
+Kopia includes unix-domain-socket support ans socket-activetion support as of v0.14.0
 
 When using socket-activation with Kopia, it is generally adviseable to enable both the socket and the service
 in systemd.  This is to because on-demand enabling could prevent maintenance tasks from running on a regular
 schedule.
-
-With the patch socket-activation is automatically detected and used when available.
 
 The following instructions build a light-weight Alpine based kopia-server container, rather than the
 default Ubuntu based container.
 
 ## Usage
 
-* Build socket-activated kopia:
-  ./build_socket_activated_kopia.sh
+* ./build_socket_activated_kopia
 * cp kopia.service kopia.socket ~/.config/systemd/user/
 * systemctl --user daemon-reload
 * systemctl --user enable kopia.socket
--- kopia/build_socket_activated_kopia.sh
+++ kopia/build_socket_activated_kopia.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 repo=kopia/kopia
 project=kopia
-patch=3283.patch
 tag=socketactivated
-branch=master
+#branch=master
 
 set -e
 if [[ -z $branch ]]; then
   if [[ "x$1" != "x" ]]; then
     ver=$1
   else
-    ver=$(curl -I -s https://github.com/$repo/releases/latest | grep '^location:' | sed -e 's|.*/||' -e 's/\r$//')
+    ver=$(curl -I -s https://github.com/$repo/releases/latest | grep '^location:' | sed -e 's|.*/v||' -e 's/\r$//')
   fi
+  vver=v$ver
   root=https://github.com/$repo/archive/refs/tags
 else
   root=https://github.com/$repo/archive/refs/heads
   ver=$branch
+  vver=$ver
 fi
-if [[ ! -f ${ver}.tar.gz ]]; then
+if [[ ! -f ${vver}.tar.gz ]]; then
   echo "Downloading version: $ver"
-  wget ${root}/${ver}.tar.gz
+  wget ${root}/${vver}.tar.gz
 fi
 rm -rf $project-$ver
-tar -xf ${ver}.tar.gz
+tar -xf ${vver}.tar.gz
 cd $project-$ver
-patch -p1 < ../$patch
 podman build --tag $project:$tag --tag $project:$ver-$tag -f ../Dockerfile .
 cd ..
 rm -rf $project-$ver
-rm ${ver}.tar.gz
+rm ${vver}.tar.gz
